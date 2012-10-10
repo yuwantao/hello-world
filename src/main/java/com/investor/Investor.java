255170db32cec51321f83210cda4f707801d4771
yuwantao <yuwantao1109@gmail.com>
2012-10-10 18:15:10 +0800

    娣诲姞constructor鍒癐nvestor.

--- src/main/java/com/investor/Investor.java
+++ src/main/java/com/investor/Investor.java
@@ -3,6 +3,10 @@ package com.investor;
 public class Investor {
     private String name;
 
+    public Investor(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }

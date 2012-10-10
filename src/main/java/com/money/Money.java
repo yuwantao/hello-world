4a6aa14e0441a9ecdf01828039275cc4108e9d64
yuwantao <yuwantao1109@gmail.com>
2012-10-10 16:21:08 +0800

    Add currency attribute to Money.java.

--- src/main/java/com/money/Money.java
+++ src/main/java/com/money/Money.java
@@ -1,7 +1,14 @@
 package com.money;
 
 public class Money {
+    private static Object currency;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        getCurrency();
+    }
+
+    public static Object getCurrency() {
+        return currency;
     }
 }

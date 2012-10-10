3cfe1bde5501441b99dd04bae75e1c45d89174b4
yuwantao <yuwantao1109@gmail.com>
2012-10-10 16:32:55 +0800

    add name to Currency

--- src/main/java/com/money/Currency.java
+++ src/main/java/com/money/Currency.java
@@ -1,5 +1,13 @@
 package com.money;
 
 public class Currency {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

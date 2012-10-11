d32ce6f5f81bc45a95ff58fc9d5ac4ab63075173
yuwantao <yuwantao1109@gmail.com>
2012-10-11 20:55:49 +0800

    dsfdf

--- src/main/java/com/money/Money.java
+++ src/main/java/com/money/Money.java
@@ -4,7 +4,7 @@ public class Money {
     private static Object currency;
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Wsdforld");
         getCurrency();
     }
 
